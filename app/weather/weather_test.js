8c76f1f9527b3603845feb34dabe19afd429bd7e
Lola <LEhyphenM@users.noreply.github.com>
2017-08-17 16:42:30 -0500

    Update weather_test.js

--- app/weather/weather_test.js
+++ app/weather/weather_test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('weatherApp.weather module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('weatherApp.weather'));
 
-  describe('view1 controller', function(){
+  describe('weatherApp controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+      var WeatherCtrl = $controller('WeatherCtrl');
+      expect(WeatherCtrl).toBeDefined();
     }));
 
   });
